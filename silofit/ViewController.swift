d653439f184b76f790ea708f465882ded002bf87
rifeng-ding <rifeng.ding@gmail.com>
2020-04-22 10:33:57 -0400

    Initial Commit

---
+++ silofit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  silofit
+//
+//  Created by Rifeng Ding on 2020-04-22.
+//  Copyright © 2020 Rifeng Ding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
